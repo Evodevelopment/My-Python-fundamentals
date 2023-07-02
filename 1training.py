2a647f06fdd22f63574949870d894192e743a53f
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-02 08:15:34 +0200

    Create 1training.py

---
+++ 1training.py
@@ -0,0 +1,10 @@
+# Programming training 2.7. 
+# Fundamental movement training
+frequency = 'everyday'
+intensity = 'low'
+
+# Power training 
+frequencypowert = '2 - 3 times a week'
+intensitypowert = 'medium to high'
+powertrainingload = 'upper body, lower body'
+
