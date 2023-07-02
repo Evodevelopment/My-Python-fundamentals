834b7339a6c9d41984b929be1e7ded866e3fce65
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-02 08:48:06 +0200

    Update 1training.py

--- 1training.py
+++ 1training.py
@@ -8,3 +8,7 @@ frequencypowert = '2 - 3 times a week'
 intensitypowert = 'medium to high'
 powertrainingload = 'upper body, lower body'
 
+if trainoverloadt
+give yourself time to rest :)
+
+Lepsi vynechat jeden trenink, nez jeden odpocinek.
