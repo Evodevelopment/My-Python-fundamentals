7c9a8482d44a7febc0a540e3c48ff444d9810962
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-09 18:45:56 +0200

    Update 1training.py

--- 1training.py
+++ 1training.py
@@ -1,5 +1,14 @@
 # Programming training 2.7. 
 # Fundamental movement training
+Frequency = 'once a week'
+intensity = 'low'
+
+highly_active = Frequency == ' daily ' 
+print("highly active user: ")
+print(highly_active)
+
+
+
 frequency = 'everyday'
 intensity = 'low'
 
@@ -11,11 +20,15 @@ powertrainingload = 'upper body, lower body'
 if trainoverloadt
 give yourself time to rest :)
 
+
+
 editace textu v pythonu
 Lepsi vynechat jeden trenink, nez jeden odpocinek.
 
 #what means 
 higly_active = frequency == 'daily'
 #need  to work on my training Power and the Sport
+
+
 
 
