37861d947719d28cff048d87f747db45a03e334e
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-09 16:48:28 +0200

    Update Joining strings randomly in Python.py

--- Joining strings randomly in Python.py
+++ Joining strings randomly in Python.py
@@ -3,3 +3,21 @@ words = ['why', 'who', 'what', 'why', 'when', 'how']
 
 for i in range(100):
     print ''.join(random.choice(words[:randint(1, 4)]))
+
+# If you don't care which element to be repeated
+for i in range(100):
+    arr = random.sample(words, random.randint(1,4))
+    # select a random element from arr and append to self     
+    arr.append(random.choice(words))
+    print ''.join(arr)
+
+# if you don't want this operation to be repeated if repetitions are already there,
+
+arr = random.sample(words, random.randint(1,4))
+# first check if array contains repetetive elements or not 
+# if contains, go and join the list, otherwise select a random element
+# from array and add to that array again  
+if not [el for el in arr if arr.count(l) > 1]:
+    arr.append(random.choice(words))                
+print ''.join(arr)
+
