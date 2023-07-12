977c69ca60a0b5a9771d7123ad65814ba0b2e2d0
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-12 13:41:12 +0200

    Update Cviceni1.py

--- Cviceni1.py
+++ Cviceni1.py
@@ -2,6 +2,10 @@ x.index(x.count(x[0]))
 
 
 
-#The .append()list method adds a new element to the  end of a list. 
-This changes the list it is applied to by extending it. 
-l.append('horse') will append the string 'horse' to the end of the list l. l = [1, 2, 3] l.append('horse') print(l) [1, 2, 3, 'horse']   
+#The 
+.append()
+#list method adds a new element to the  end of a list. 
+#This changes the list it is applied to by extending it. 
+l.append('horse')
+#will append the string 'horse' to the end of the list 
+l. l = [1, 2, 3] l.append('horse') print(l) [1, 2, 3, 'horse']   
