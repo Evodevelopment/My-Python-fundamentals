c73c9c3bd5ae30c27dd01f0ffa0e7e7de08c39bb
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-19 16:06:35 +0200

    Update Pythonic.py

--- Pythonic.py
+++ Pythonic.py
@@ -21,3 +21,15 @@ print(best_list)
 
 #The Zen of Python, by Tim Peters
 import this
+
+# Create a range object that goes from 0 to 5
+nums = range(6)
+print(type(nums))
+
+# Convert nums to a list
+nums_list = list(nums)
+print(nums_list)
+
+# Create a new list of odd numbers from 1 to 11 by unpacking a range object
+nums_list2 = [*range(1,12,2)]
+print(nums_list2)
