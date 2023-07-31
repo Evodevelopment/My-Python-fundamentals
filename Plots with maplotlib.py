c088e1eac9d7d79d01873f532fdefa781f081689
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-31 20:41:02 +0200

    Update Plots with maplotlib.py

--- Plots with maplotlib.py
+++ Plots with maplotlib.py
@@ -1,1 +1,9 @@
-Upload your code
+#Basic matplotlib XY graph
+# Import the matplotlib.pyplot submodule and name it plt
+import matplotlib.pyplot as plt
+
+# Create a Figure and an Axes with plt.subplots
+fig, ax  = plt.subplots()
+
+# Call the show function to show the result
+plt.show()
