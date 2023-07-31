54e77fb7c769a760c32aecc8c36c707498bc54c7
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-31 20:44:48 +0200

    Update Plots with maplotlib.py

--- Plots with maplotlib.py
+++ Plots with maplotlib.py
@@ -7,3 +7,18 @@ fig, ax  = plt.subplots()
 
 # Call the show function to show the result
 plt.show()
+
+# Import the matplotlib.pyplot submodule and name it plt
+import matplotlib.pyplot as plt
+
+# Create a Figure and an Axes with plt.subplots
+fig, ax = plt.subplots
+
+# Plot MLY-PRCP-NORMAL from seattle_weather against the MONTH
+ax.(seattle_weather["MONTH"], ____)
+
+# Plot MLY-PRCP-NORMAL from austin_weather against MONTH
+ax.____(____, ____)
+
+# Call the show function
+____
