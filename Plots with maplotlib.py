8101088dc8845542e9bd971466f1843e3bea72f0
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-02 20:40:52 +0200

    Update Plots with maplotlib.py

--- Plots with maplotlib.py
+++ Plots with maplotlib.py
@@ -5,6 +5,17 @@ pracuju na svem potucku
 stava se z nej reka
 co tece do daleka
 
+
+Dobrý den, 
+
+rád bych u Vás pracoval na (pozice), rád bych se zlepsil v dane cinnosti.
+  Jsem si plně vědom toho, že na svých dovednostech musím pracovat a je potřeba se jim pilně věnovat.
+  Rád bych se posunul a pracoval na sobě.
+  Chci být šikovný. Pracuji na sobě.
+  Bla bla bla
+Proste chci praci, splnuji zakladni dovednosti ktere chci dale rozvijet, tecka
+Reference na me vam muze dat Jana Zamecnikova, s niz jsem měl tu čest pracovat v FNB
+
 #Basic matplotlib XY graph
 # Import the matplotlib.pyplot submodule and name it plt
 import matplotlib.pyplot as plt
