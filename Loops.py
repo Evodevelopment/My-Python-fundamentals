aade7d86fca8f6774de4b23e9bbe2b5ca768d798
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-04 17:07:30 +0200

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,5 +1,12 @@
 #Udemy course outline
-
+#enumarate
 
 #Special   built in loop function - range():
 print(range(0, 100))
+
+for item in range (0, 100):
+  print(item)
+#I dont care for the varible name I just want to loop over the range:
+for _ in range (100):
+print(_)
+
