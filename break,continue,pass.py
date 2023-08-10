7d23ab270262550d05b29e4f40592d4835aafbe7
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-10 11:07:53 +0200

    Create break,continue,pass.py

---
+++ break,continue,pass.py
@@ -0,0 +1,13 @@
+my_list = [1,2,3]
+for item in my_list:
+  pass
+  print(item)
+  #thing about it
+  pass
+  
+  i = 0
+  while i < len (my_list):
+    print(my_list)
+    i += 1
+    pass
+    break
