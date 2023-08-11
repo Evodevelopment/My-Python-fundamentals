a939596bf106241d26b125eb4c8d7accffee91b6
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-11 11:01:41 +0200

    Create First GUI.py

---
+++ First GUI.py
@@ -0,0 +1,24 @@
+#Exercise
+
+picture = [
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+]
+
+print()
+# Iterate over the picture
+  # if 0 -> print ''
+  # if 1 -> print *
+
+for row in picture:
+  for pixel in image:
+    if (pixel == 1):
+      print ('*' end= '')
+    else:
+      print(' ', end= '')
+  print('')
