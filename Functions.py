597787d8e9af2bd21e98943994ee582fb924becf
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-09-13 09:22:05 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,1 +1,7 @@
+# we use functions to create whatever we want, we use the () - brackets to notify the interpretre we want to perform faction on, in repl functions is represented by purple box
+def say_hello():
+  print("Hellooooo")
 
+say_hellp()
+
+def function1
