50ba8494b6cd8b9803f4848785169f60a2b7e6e1
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-12-18 10:56:19 +0100

    Update Checkfor duplicates.py

--- Checkfor duplicates.py
+++ Checkfor duplicates.py
@@ -3,16 +3,4 @@ some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 
 For item in some_list:
 
-
 #edit the list with Udemy
-
-the video... continue :)
-
-Ask AI copilot and gpt as well
-
-And dont forget to have a good night sleep
-
-You can write some for cycles, pythonic poems about it as well
-
-Lol
-
