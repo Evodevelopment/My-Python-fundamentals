704d48eb65fde9fb0368f383ab8b7f1bd27b700f
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-12-28 17:27:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+
+def do_this():
+  # simulating running code
+  time.sleep(1.3)
+
+def do_that():
+  # simulating running code
+  time.sleep(1.4)
+
+do_this()
+do_that()
+print('Done')
+  
