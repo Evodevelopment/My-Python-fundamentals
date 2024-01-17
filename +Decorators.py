1a56e0f96ff11d56ba96e0c148b39252cfe54bb9
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2024-01-17 10:04:14 +0100

    Update +Decorators.py

--- +Decorators.py
+++ +Decorators.py
@@ -2,5 +2,15 @@ def hello():
   print("Helllooooooo")
 
 greet = hello
-
+#del hello - deletes the name function but in Python memory stays the function itseld
 print(greet(()
+
+def hello_1(func):
+    func ()
+
+def greet():
+  print("still here!")
+
+a = hello(greet)
+
+print (a)
