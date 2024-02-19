8cd505522eb7cc85b2bed85c7711a26434663593
JDrepos <66202511+Evodevelopment@users.noreply.github.com>
2024-02-19 10:21:23 +0100

    Update Pyramid.py

--- PatternPrograms/Pyramid.py
+++ PatternPrograms/Pyramid.py
@@ -4,3 +4,5 @@ def print_pyramid(rows):
 
 # Example usage:
 print_pyramid(5)
+
+# In the example where we call print_pyramid(5), rows is 5, so the loop will iterate over the range 0, 1, 2, 3, 4. This means i will take values from 0 to 4.
